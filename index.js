5f772cfb88504c501af13aacd662c7c7e8c0f1c3
mikhail.konoval <mikhail.konoval@moneyveo.com>
2021-03-03 20:56:32 +0200

    tets

--- index.js
+++ index.js
@@ -4,21 +4,37 @@ const app = express()
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.end(`<h1>Hello World</h1>`)
+  res.end(`
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="alternate" href="android-app://com.moneyveo/https/moneyveo.ua" />
+</head>
+<body>
+
+    <h1>HTML HOME PAGE</h1>
+
+    <a href="/apple-app-site-association">ios</a>
+
+</body>
+</html>
+  `)
 })
 
 
-app.get('/about', (req, res) => {
-  res.end(`<h1>About World</h1>`)
-})
 
-
-const some = require("./some.json")
-app.get('/some.json', (req, res) => {
-  res.end(some)
+const some = require("./apple-app-site-association.json")
+const str = JSON.stringify(some)
+app.get('/apple-app-site-association', (req, res) => {
+  console.log(some)
+  // res.end(some)
+  res.json(some)
 })
 
 app.listen(PORT, () => {
-
-  console.log('APP STARTED  !!! ---')
+  console.log('APP STARTED  !!! --- http://localhost:8080')
 })
