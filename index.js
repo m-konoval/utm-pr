d450b62e663d0ea6d8ebf07bd39009df4531dce8
mikhail.konoval <mikhail.konoval@moneyveo.com>
2021-03-03 11:21:33 +0200

    qweq

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.end(`<h1>Hello World</h1>`)
+})
+
+
+app.get('/about', (req, res) => {
+  res.end(`<h1>About World</h1>`)
+})
+
+app.listen(PORT, () => {
+
+  console.log('APP STARTED  !!! ---')
+})
