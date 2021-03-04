6c0c36da9b79a49300694dfd4b4f69c989ca0ac3
mikhail.konoval <mikhail.konoval@moneyveo.com>
2021-03-04 13:51:06 +0200

    dd

---
+++ homeTemplate.js
@@ -0,0 +1,21 @@
+const home = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="alternate" href="android-app://com.moneyveo/https/moneyveo.ua" />
+</head>
+<body>
+
+    <h1>HTML HOME PAGE</h1>
+
+    <a href="/apple-app-site-association">ios</a>
+
+</body>
+</html>
+`
+
+module.exports = home
--- index.js
+++ index.js
@@ -3,35 +3,15 @@ const app = express()
 
 const PORT = process.env.PORT || 8080;
 
+
+const home = require('./homeTemplate')
 app.get('/', (req, res) => {
-  res.end(`
-  <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="alternate" href="android-app://com.moneyveo/https/moneyveo.ua" />
-</head>
-<body>
-
-    <h1>HTML HOME PAGE</h1>
-
-    <a href="/apple-app-site-association">ios</a>
-
-</body>
-</html>
-  `)
+  res.end(home)
 })
 
 
-
 const some = require("./apple-app-site-association.json")
-const str = JSON.stringify(some)
 app.get('/apple-app-site-association', (req, res) => {
-  console.log(some)
-  // res.end(some)
   res.json(some)
 })
 
