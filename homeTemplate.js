1ddf8c3ef50bfb9b455f7ac977021b4a66d549f1
mikhail.konoval <mikhail.konoval@moneyveo.com>
2021-03-11 17:23:41 +0200

    an

--- homeTemplate.js
+++ homeTemplate.js
@@ -7,6 +7,7 @@ const home = `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="alternate" href="android-app://com.moneyveo/https/moneyveo.ua" />
+    <link rel="alternate" href="android-app://com.moneyveo/https/utm-test-pr.herokuapp.com" />
 </head>
 <body>
 
